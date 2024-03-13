713ed82af4901b2edd9b7b44120a583583cafc77
Vinicius Gomes <ViniiciusGomes27@gmail.com>
2024-03-13 20:15:30 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,205 @@
+const menu = document.getElementById("menu");
+const cartBtn = document.getElementById("cart-btn");
+const cartModal = document.getElementById("cart-modal");
+const cartItemsContainer = document.getElementById("cart-items");
+const cartTotal = document.getElementById("cart-total");
+const checkoutBtn = document.getElementById("checkout-btn");
+const closeModalBtn = document.getElementById("close-modal-btn");
+const cartCounter = document.getElementById("cart-count");
+const addressInput = document.getElementById("address");
+const addressWarn = document.getElementById("address-warn");
+
+let cart = [];
+
+// Open Cart Modal
+cartBtn.addEventListener("click", function()    {
+    updateCartModal();
+    cartModal.style.display = "flex";
+});
+
+// Close Cart Modal
+cartModal.addEventListener("click", function()  {
+    if(event.target === cartModal){
+        cartModal.style.display = "none";
+    }
+});
+
+// Close Cart with Chiudi
+closeModalBtn.addEventListener("click", function()  {
+    cartModal.style.display = "none";
+});
+
+// Take the attributes name and price
+menu.addEventListener("click", function(event) {
+    let parentBtn = event.target.closest(".add-to-cart-btn");
+
+    if (parentBtn) {
+        const name = parentBtn.getAttribute("data-name");
+        const price = parseFloat(parentBtn.getAttribute("data-price"));
+
+        addToCart(name, price);
+    }
+});
+
+// Add to cart
+function addToCart(name, price) {
+    const existingItem = cart.find(item => item.name === name);
+
+    if (existingItem) {
+        existingItem.quantity += 1;
+    } else {
+        cart.push({
+            name,
+            price,
+            quantity: 1,
+        });
+    }
+ 
+    Toastify({
+        text: "Aggiunto al Carrello",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: "#7CFC00",
+        },
+    }).showToast();
+
+    updateCartModal();
+}
+
+function updateCartModal()  {
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div");
+        cartItemElement.classList.add("flex", "justify-content", "mb-4", "flex-col");
+        cartItemElement.innerHTML = `
+        <div class="flex items-center justify-between">
+            <div>
+                <p class="font-medium">${item.name}</p>
+                <p>Qtd: ${item.quantity}</p>
+                <p class="font-medium mt-2">€ ${item.price.toFixed(2)}</p>
+            </div>
+            <div>
+                <button class="p-2 bg-red-500 text-white font-medium rounded-md remove-btn" data-name="${item.name}">
+                    Delete
+                </button>
+            </div>
+        </div>
+        `;
+
+        total += item.price * item.quantity;
+
+        cartItemsContainer.appendChild(cartItemElement);
+    });
+    cartTotal.textContent = total.toLocaleString("it-IT", {
+        style: "currency",
+        currency: "EUR"
+    });
+
+    cartCounter.innerHTML = cart.length;
+}
+
+// Remove Item from the Modal
+cartItemsContainer.addEventListener("click", function(event){
+    if(event.target.classList.contains("remove-btn")){
+        const name = event.target.getAttribute("data-name");
+
+        removeItemCart(name);
+        console.log(name);
+    }
+
+    function removeItemCart(name) {
+        const index = cart.findIndex(item => item.name === name);
+
+        if (index !== -1) {
+            const item = cart[index];
+            console.log(item);
+            if (item.quantity > 1) {
+                item.quantity -= 1;
+                updateCartModal();
+                return;
+            }
+
+            cart.splice(index, 1);
+            updateCartModal();
+        }
+    }
+});
+
+addressInput.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+
+    if(inputValue !== ""){
+        addressInput.classList.remove("border-red-500");
+        addressWarn.classList.add("hidden");
+    }
+});
+
+
+// Verification address
+checkoutBtn.addEventListener("click", function()    {
+    const isOpen = checkRestaurantOpen();
+    if(!isOpen){
+        Toastify({
+            text: "La Dev Burger è chiusa!!",
+            duration: 3000,
+            destination: "https://github.com/apvarun/toastify-js",
+            newWindow: true,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "#ef4444",
+            },
+        }).showToast();
+        return;
+    }
+
+    if(cart.length === 0) return;
+    if(addressInput.value === ""){
+        addressWarn.classList.remove("hidden");
+        addressInput.classList.add("border-red-500");
+        return;
+    }
+
+    const cartItems = cart.map((item) => {
+        return(
+            `${item.name} Quantità: (${item.quantity}) Totale: € ${item.price} `
+        )
+    }).join("");
+
+    const message = encodeURIComponent(cartItems);
+    const phone = "";
+
+    window.open(`https://wa.me/${phone}?text=${message} Indirizzo: ${addressInput.value}`, "_blank");
+
+
+    cart = [];
+    updateCartModal();
+});
+
+// Verification hour
+function checkRestaurantOpen()  {
+    const data = new Date();
+    const hour = data.getHours();
+    return hour >= 18 && hour < 22;
+}
+
+const spanItem = document.getElementById("date-span")
+const isOpen = checkRestaurantOpen();
+
+if(isOpen){
+    spanItem.classList.remove("bg-red-500");
+    spanItem.classList.add("bg-green-600");
+} else {
+    spanItem.classList.remove("bg-green-600");
+    spanItem.classList.add("bg-red-500");
+}
